b61ddda344f5cc6d6f5e205b3a516fc9aa7f4bab
yamunasreeoggu <yamunasree321@gmail.com>
2023-10-02 18:13:44 -0400

    vpc network creation

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -45,3 +45,27 @@ resource "aws_nat_gateway" "ngw" {
     Name = "${var.env}-ngw"
   }
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "public"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ngw.id
+  }
+  tags = {
+    Name = "private"
+  }
+}
