79b2266834847b3f039eb175553902b706123c93
yamunasreeoggu <yamunasree321@gmail.com>
2023-10-02 18:03:45 -0400

    vpc network creation

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -24,3 +24,24 @@ resource "aws_subnet" "private_subnets" {
     Name = "private-subnet-${count.index+1}"
   }
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.env}-igw"
+  }
+}
+
+resource "aws_eip" "eip" {
+  domain   = "vpc"
+}
+
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.eip.id
+  subnet_id     = aws_subnet.public_subnets[0].id
+
+  tags = {
+    Name = "${var.env}-ngw"
+  }
+}
