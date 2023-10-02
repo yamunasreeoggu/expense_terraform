27ba5c266188efad3ba2e75e15be4f16873ddd10
yamunasreeoggu <yamunasree321@gmail.com>
2023-10-02 19:21:17 -0400

    vpc network creation

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -90,3 +90,15 @@ resource "aws_route" "default-route-table" {
   destination_cidr_block    = var.vpc_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
 }
+
+resource "aws_route_table_association" "public" {
+  count      = length(var.public_subnets)
+  subnet_id      = aws_subnet.public_subnets[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  count      = length(var.private_subnets)
+  subnet_id      = aws_subnet.private_subnets[count.index].id
+  route_table_id = aws_route_table.private.id
+}
