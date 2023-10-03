21f75fb631beeb11d665122619f871fa06534678
yamunasreeoggu <yamunasree321@gmail.com>
2023-10-03 19:11:55 -0400

    SSH allow

--- modules/app/userdata.sh
+++ modules/app/userdata.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-dnf install python3.11-pip -y
-pip3.11 install botocore boto3
-ansible-pull -i localhost, -U https://github.com/yamunasreeoggu/infra_ansible main.yml -e role_name=${role_name}
+dnf install python3.11-pip ansible -y | tee -a /opt/userdata.log
+pip3.11 install botocore boto3 | tee -a /opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/yamunasreeoggu/infra_ansible main.yml -e role_name=${role_name} | tee -a /opt/userdata.log
