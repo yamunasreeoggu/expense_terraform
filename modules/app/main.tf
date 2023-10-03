f8ad59674a5ee1605237ce7b1300a5bd92fa2cd3
yamunasreeoggu <yamunasree321@gmail.com>
2023-10-03 19:39:23 -0400

    SSH allow

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -30,11 +30,59 @@ resource "aws_security_group" "security_group" {
   }
 }
 
+resource "aws_iam_role" "role" {
+  name = "${var.env}-${var.component}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  inline_policy {
+    name = "${var.env}-${var.component}-policy"
+
+    policy = jsonencode({
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Sid": "VisualEditor0",
+          "Effect": "Allow",
+          "Action": [
+            "ssm:DescribeParameters",
+            "ssm:GetParameterHistory",
+            "ssm:GetParametersByPath",
+            "ssm:GetParameters",
+            "ssm:GetParameter"
+          ],
+          "Resource": "*"
+        }
+      ]
+    })
+  }
+
+  tags = {
+    tag-key = "${var.env}-${var.component}-role"
+  }
+}
+
 resource "aws_launch_template" "template" {
   name                   = "${var.env}-${var.component}"
   image_id               = data.aws_ami.ami.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.security_group.id]
+  iam_instance_profile {
+    name = aws_iam_role.role.name
+  }
+
   user_data              = base64encode(templatefile("${path.module}/userdata.sh", {
     role_name            = var.component
   }))
