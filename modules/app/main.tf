443f224fb6ec6cdfdac23e788e5f0f2c93ea053b
yamunasreeoggu <yamunasree321@gmail.com>
2023-10-03 19:54:35 -0400

    SSH allow

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -74,13 +74,18 @@ resource "aws_iam_role" "role" {
   }
 }
 
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = "${var.env}-${var.component}-role"
+  role = aws_iam_role.role.name
+}
+
 resource "aws_launch_template" "template" {
   name                   = "${var.env}-${var.component}"
   image_id               = data.aws_ami.ami.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.security_group.id]
   iam_instance_profile {
-    name = aws_iam_role.role.name
+    name = aws_iam_instance_profile.instance_profile.name
   }
 
   user_data              = base64encode(templatefile("${path.module}/userdata.sh", {
