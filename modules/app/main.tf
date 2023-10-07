b3b6dc1d9ec2bf2fe5fce8a8d592490a240c6ae9
yamunasreeoggu <yamunasree321@gmail.com>
2023-10-07 16:11:34 -0400

    creating tg

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -120,4 +120,12 @@ resource "aws_lb_target_group" "tg" {
   port     = var.app_port
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+  health_check {
+    enabled = true
+    healthy_threshold = 2
+    interval = 5
+    unhealthy_threshold = 2
+    port = var.app_port
+    path = "/health"
+  }
 }
