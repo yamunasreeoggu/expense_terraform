e48a152ee0c94456033f75287fa4cbc7df56c9cb
yamunasreeoggu <yamunasree321@gmail.com>
2023-10-08 17:54:48 -0400

    creating tg

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -51,7 +51,25 @@ resource "aws_route53_record" "www" {
   records = [aws_lb.alb.dns_name]
 }
 
-resource "aws_lb_listener" "listener_http" {
+resource "aws_lb_listener" "listener_http-public" {
+  count             = var.alb_type == "public" ? 1 : 0
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "listener_http-private" {
+  count             = var.alb_type == "private" ? 1 : 0
   load_balancer_arn = aws_lb.alb.arn
   port              = "80"
   protocol          = "HTTP"
