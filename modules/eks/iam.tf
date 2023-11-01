1aa35a11e7a18983b9f9f6cfa81a6499e333bc58
yamunasreeoggu <yamunasree321@gmail.com>
2023-11-01 17:51:19 -0400

    creating eks cluster

--- modules/eks/iam.tf
+++ modules/eks/iam.tf
@@ -63,7 +63,7 @@ resource "aws_iam_openid_connect_provider" "eks" {
   url = aws_eks_cluster.main.identity[0].oidc[0].issuer
 
   client_id_list = [
-    split("/", aws_eks_cluster.main.identity[0].oidc[0].issuer)[4]
+    "sts.amazonaws.com"
   ]
 
   thumbprint_list = [lookup(data.external.thumbprint.result, "thumbprint", null)]
